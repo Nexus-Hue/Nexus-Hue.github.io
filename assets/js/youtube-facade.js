f595c179d1298d21cd2d88a8ed7a99bd1ab55eda
Nexus Hue <nexushuestudio@gmail.com>
2025-04-20 19:55:22 -0700

    Youtube Facade Script Update
    This update to the scrip is supposed to help the video load without having to click twice on the video after clicking the facade.

--- assets/js/youtube-facade.js
+++ assets/js/youtube-facade.js
@@ -1,6 +1,32 @@
 document.querySelectorAll('.youtube-facade').forEach(function(el){
   el.addEventListener('click', function(){
     var vid = el.getAttribute('data-video');
-    el.outerHTML = `<iframe width="100%" height="263" src="https://www.youtube.com/embed/${vid}?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen loading="lazy" style="border-radius:15px;"></iframe>`;
+    
+    // Create the iframe element
+    var iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = '263';
+    iframe.src = `https://www.youtube.com/embed/${vid}?autoplay=1&enablejsapi=1`;
+    iframe.frameBorder = '0';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+    iframe.allowFullscreen = true;
+    iframe.loading = 'lazy';
+    iframe.style.borderRadius = '15px';
+    
+    // Replace the facade with the iframe
+    el.innerHTML = '';
+    el.appendChild(iframe);
+    
+    // Try to programmatically play the video after a slight delay
+    setTimeout(function() {
+      try {
+        // This attempts to interact with YouTube's iframe API
+        if (iframe.contentWindow) {
+          iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        }
+      } catch(e) {
+        console.log('YouTube autoplay attempt:', e);
+      }
+    }, 300);
   });
 });

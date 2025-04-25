b5b98ccd0ba0272d93486d2e79cbbd3493623701
Nexus Hue <nexushuestudio@gmail.com>
2025-04-25 16:45:28 -0700

    Book Bug Killed
    -The script now also closes the book when u click the inside pages for mobile.

--- assets/js/BookStyleJutsu.js
+++ assets/js/BookStyleJutsu.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('.book').forEach(function (book) {
-      book.addEventListener('click', function () {
-        // Toggle 'open' class on tap
+  document.querySelectorAll('.book').forEach(function (book) {
+    book.addEventListener('click', function (e) {
+      // Prevents nested clicks from bubbling and triggering unintended closes
+      if (e.target.closest('.book')) {
         book.classList.toggle('open');
-      });
+      }
     });
   });
+});

6b937baaf02208d4e7921bfdc0e271169ec2504c
Nexus Hue <nexushuestudio@gmail.com>
2026-02-07 14:01:05 -0800

    Illustration protfolio images were not loading in properly
    -Removed the lazy loading and low resolution img to avoid letting that shit get stuck on some phones

--- assets/js/brand-slider.js
+++ assets/js/brand-slider.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const brandSlider = document.querySelector(".brand-slider");
-	if (brandSlider) {
-		new Swiper(".brand-slider", {
-			lazy: { loadPrevNext: !0, loadPrevNextAmount: !0 },
-			loop: !0,
-			breakpoints: {
-				390: { slidesPerView: 1, spaceBetween: 15 },
-				640: { slidesPerView: 2, spaceBetween: 20 },
-				768: { slidesPerView: 2, spaceBetween: 50 },
-				1024: { slidesPerView: 3, spaceBetween: 50 },
-				1920: { slidesPerView:4, spaceBetween: 50 },
-				2000: { slidesPerView:7, spaceBetween: 57 }
-			},
-			navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }
-		});
-	}
+    const brandSlider = document.querySelector(".brand-slider");
+    
+    if (brandSlider) {
+        new Swiper(".brand-slider", {
+            // lazy loading logic removed for native browser performance
+            loop: true, 
+            breakpoints: {
+                390: { slidesPerView: 1, spaceBetween: 10 },
+                640: { slidesPerView: 2, spaceBetween: 20 },
+                768: { slidesPerView: 2, spaceBetween: 50 },
+                1024: { slidesPerView: 3, spaceBetween: 50 },
+                1920: { slidesPerView: 4, spaceBetween: 50 },
+                2000: { slidesPerView: 7, spaceBetween: 57 }
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            }
+        });
+    }
 });
